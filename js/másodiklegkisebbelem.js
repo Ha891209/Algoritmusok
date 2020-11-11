596d5928db85668b83071f049fd15b0714c4b679
Aniko Kopis-Horvath <ha891209@gmail.com>
2020-11-11 18:06:06 +0100

    másodiklegkisebbelem kész

---
+++ js/másodiklegkisebbelem.js
@@ -0,0 +1,33 @@
+const intArr = [1989,1990,2020,12,9,1,10,4];
+
+function secondSmallestElement(arr) {
+
+    let smallest = arr[0];
+    let secondSmallest = arr[0];
+
+    for (let i = 1; i < arr.length; i += 1) {
+        if (arr[i] < smallest) {
+            secondSmallest = smallest;
+            smallest = arr[i];
+        } else {
+            if (arr[i] < secondSmallest) {
+                secondSmallest = arr[i];
+            }
+        }
+    }
+    return secondSmallest;
+}
+
+console.log(secondSmallestElement(intArr));
+
+
+
+
+
+
+
+
+
+
+
+
