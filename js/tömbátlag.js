42b3911ccbe235150cf91b004f565bdb1ce482bd
Aniko Kopis-Horvath <ha891209@gmail.com>
2020-11-11 13:42:27 +0100

    tömbátlag kész

---
+++ js/tömbátlag.js
@@ -0,0 +1,13 @@
+const intArr = [1989,1990,2020,12,9,1,10,4];
+
+function avgArray(arr) {
+    var sum = 0;
+    for (var i = 0, j = arr.length; i < j; i++) {
+      sum += arr[i];
+    }
+    return sum / arr.length;
+  }
+  
+    
+    console.log(avgArray(intArr));
+    
