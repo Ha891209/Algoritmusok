2c56778ee358de92bbb78d4789a5a902eb001e61
Aniko Kopis-Horvath <ha891209@gmail.com>
2020-11-11 14:14:14 +0100

    tömbösszeg kész

---
+++ js/tömbösszeg.js
@@ -0,0 +1,10 @@
+const intArr = [1989,1990,2020,12,9,1,10,4];
+
+function osszeg(arr){
+    let osszeg=0;
+    for(let i=0; i<arr.length; i += 1){
+        osszeg = osszeg + arr[i];
+    }
+    return osszeg
+}
+console.log(osszeg(intArr));
