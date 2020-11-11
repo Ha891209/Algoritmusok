755125897367344ec4dcabfd5c8d93e0c3f6f832
Aniko Kopis-Horvath <ha891209@gmail.com>
2020-11-11 13:28:53 +0100

    legnagyobbelem kész

---
+++ js/legnagyobbelem.js
@@ -0,0 +1,14 @@
+const intArr = [1989,1990,2020,12,9,1,10,4];
+
+function BiggestElement(arr) {
+    let biggest = arr[0];
+    for (let i = 1; i < arr.length; i += 1) {
+        if (arr[i] > biggest) {
+            biggest = arr[i];
+        }
+    }
+
+    return biggest
+  
+}
+console.log(BiggestElement(intArr));
