b3546dc607db02a99c182ab15a8a7019d2b633cf
Ha891209 <ha891209@gmail.com>
2020-11-22 15:59:57 +0100

    változtatásokkal kész

--- js/23tartalmazzae2.js
+++ js/23tartalmazzae2.js
@@ -1,18 +1,40 @@
 //Írd ki (igen vagy nem), hogy egy tetszőlegese elemszámú, csak egész számokat tartalmazó tömb tartalmazza-e a 23-as számot! Használj logaritmikus keresést! Nézz utána mi az, milyen feltételei vannak, és hogyan valósítható meg!
-
 const intArr = [1989,1990,2020,12,9,1,10,4];
+const sortedArray = bubbleSort(intArr)
 
-let numberToFind = 23;
-let found = false;
-
-function linearSearch(arr, toFind) {
-    for (let i = 0; i < arr.length; i += 1) {
-        if (arr[i] == toFind) {
-            found = true;
-            break;
+function binarySearch(arr,search) {
+    let start = 0;
+    let end = arr.length -1;
+    while (start <=end) {
+        let mid = Math.floor((start+end) /2);
+        if (arr[mid] === search) {
+            return true;
+        }
+        else if (arr[mid]< search){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
         }
     }
-    return found;
+    return false;
 }
 
-console.log(linearSearch(intArr, numberToFind));
+function bubbleSort(arr) {
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length - 1; i += 1) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped)
+    return arr;
+}
+
+console.log(binarySearch(sortedArray,23)); 
+
