b4f6cd91a29c47e11885ce29145fdf6b2e0b1625
Aniko Kopis-Horvath <ha891209@gmail.com>
2020-11-23 22:33:04 +0100

    OK

---
+++ js/16.feladat.js
@@ -0,0 +1,55 @@
+const intArr = [1989,2020,9,1,10];
+const intArr2 = [3,7,23,235,2]
+
+function union(arr1, arr2) {
+    // return arr1.concat(arr2);
+    const union = [];
+    for (let i = 0; i < arr1.length; i += 1) {
+        if (union.indexOf(arr1[i]) === -1) {
+            union.push(arr1[i]);
+        }
+        if (union.indexOf(arr2[i]) === -1) {
+            union.push(arr2[i]);
+        }
+    }
+    return union;
+}
+
+function section(arr1, arr2) {
+    const section = [];
+    for (let i = 0; i < arr1.length; i += 1) {
+        if (arr1.indexOf(arr2[i]) !== -1) {
+            section.push(arr2[i]);
+        }
+    }
+    return section
+}
+
+function difference(arr1, arr2) {
+    const defference = [];
+    for (let i = 0; i < arr1.length; i += 1) {
+        if (arr2.indexOf(arr1[i]) === -1) {
+            defference.push(arr1[i]);
+        }
+    }
+    return defference;
+}
+
+function descartes(arr1, arr2) {
+    const descartes = [];
+    // 2 darab 5 elemű tömb
+    // i = 0
+    // i = 1
+    for (let i = 0; i < arr1.length; i += 1) {
+        // j = 0, 1, 2, 3, 4 
+        // j = 0, 1, 2, 3, 4 
+        for (let j = 0; j < arr2.length; j += 1) {
+            descartes.push(arr1[i] * arr2[j]);
+        }
+    }
+    return descartes;
+}
+            console.log(union(intArr, intArr2));
+            console.log(section(intArr, intArr2));
+            console.log(difference(intArr, intArr2));
+            console.log(descartes(intArr, intArr2));
