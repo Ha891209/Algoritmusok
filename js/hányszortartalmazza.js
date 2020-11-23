7c531404dcc91a6c21bfa112ebbb8bca0de40c20
Aniko Kopis-Horvath <ha891209@gmail.com>
2020-11-23 22:00:42 +0100

    módosítva

--- js/hányszortartalmazza.js
+++ js/hányszortartalmazza.js
@@ -1,19 +1,16 @@
 //Írd ki, hogy egy tetszőlegese elemszámú, bármilyen típusú elemeket tartalmazó tömb (feltételezzük, hogy beágyazott tömböt, objektumot nem tartalmaz) hányszor tartalmazza a 23-as számot!
 
 const intArr = [1989,1990,2020,12,9,1,10,4];
+const searchValue = 23;
 
-function sameSearch(arr,cell){
-    let db=0;
-    for(let i=0; i<arr.length ;i += 1){
-        
-        if(arr[i]===cell){
-            db +=1;
+function counter(arr, search) {
+    let counter = 0;
+    for (let i = 0; i < arr.length; i += 1) {
+        if (arr[i] === search) {
+            counter += 1;
         }
     }
-    return db
-
-
-    
+    return counter;
 }
 
-console.log(sameSearch(intArr, 23));
+console.log(counter(intArr, 23));
