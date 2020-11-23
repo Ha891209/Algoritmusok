8d1fe92a76456616ca89be8de1f71e2bd872e8e0
Aniko Kopis-Horvath <ha891209@gmail.com>
2020-11-23 22:37:01 +0100

    OK

--- js/14.feladat.js
+++ js/14.feladat.js
@@ -2,45 +2,23 @@
 
 const intArr = [1989,1990,"szia",2020,12,9,1,10,4, "hello"];
 
-function customSort(arr) {
-  const numbers = [];
-  const strings = [];
-  const sortedArray = [];
+function getElementsByType(arr, type) {
+  const elements = [];
   for (let i = 0; i < arr.length; i += 1) {
-    if (typeof arr[i] === 'number') {
-      numbers.push(arr[i]);
-    } else {
-      strings.push(arr[i]);
-    }
+      if (typeof arr[i] === type) {
+          elements.push(arr[i])
+      }
   }
-  for (let i = 0; i < numbers.length; i += 1) {
-    sortedArray.push(numbers[i]);
-    sortedArray.push(strings[i]);
-  }
-  return sortedArray;
+  return elements;
 }
 
-const result = customSort(intArr);
-console.log(result);
-
-function getPrimitiveElementsByType(arr, type) {
-    const typedArray = [];
-    for (let i = 0; i < arr.length; i += 1) {
-      if (typeof arr[i] === type) {
-        typedArray.push(arr[i]);
-      }
-    }
-    return typedArray;
+function manualSort(arr) {
+  const strings = getElementsByType(arr, 'string');
+  const numbers = getElementsByType(arr, 'number');
+  const sortedArr = [];
+  for (let i = 0; i < strings.length; i += 1) {
+      sortedArr.push(numbers[i], strings[i]);
   }
-  
-  function mixArrays(arr1, arr2) {
-    const mixedArray = [];
-    for (let i = 0; i < arr1.length; i += 1) {
-      mixedArray.push(arr1[i], arr2[i]);
-    }
-    return mixedArray;
-  }
-  
-  const numbers = getPrimitiveElementsByType(intArr, 'number');
-  const strings = getPrimitiveElementsByType(intArr, 'string');
-  console.log(mixArrays(numbers, strings));
+  return sortedArr;
+}
+console.log(manualSort(intArr))
